2a46f671d72b7c65e23b3a4beb81101a514393d7
Nta1510 <125479251+Nta1510@users.noreply.github.com>
2025-08-19 00:27:37 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import RoomDetail from './pages/RoomDetail';
 import Booking from './pages/Booking';
+import PaymentConfirmation from './pages/PaymentConfirmation';
+import MyOrders from './pages/MyOrders';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/rooms/:id" element={<RoomDetail />} />
       <Route path="/booking" element={<Booking />} />
+      
+      <Route path="/booking/confirm" element={<PaymentConfirmation />} />
+      <Route path="/orders" element={<MyOrders />} />
     </Routes>
   )
 }
